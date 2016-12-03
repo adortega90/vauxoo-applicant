fa2f98ee88868ec15c6fbde7e0b8ef195359487d
adortega90 <adortega90@gmail.com>
2016-12-03 19:06:28 -0430

    creating tables of employee and hobbies

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -29,3 +29,43 @@ INSERT INTO reto.employee VALUES (3, 'Oriana', 'Castillo',4);
 INSERT INTO reto.employee VALUES (4, 'Cesar', 'Rodriguez',3);
 
 
+--
+
+CREATE TABLE reto.hobbies(
+ 	id serial PRIMARY KEY,
+ 	name VARCHAR (255) UNIQUE NOT NULL,
+	description VARCHAR (350) NOT NULL
+	  
+);
+
+CREATE TABLE reto.employee_performs_hobbies
+(
+  employee_id integer NOT NULL,
+  hobbies_id integer NOT NULL,
+  PRIMARY KEY (employee_id, hobbies_id),
+  CONSTRAINT employee_performs_hobbies_id_fkey FOREIGN KEY (hobbies_id)
+      REFERENCES reto.hobbies (id) MATCH SIMPLE
+       ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT employee_performs_hobbies_employee_id_fkey FOREIGN KEY (employee_id)
+      REFERENCES reto.employee (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+INSERT INTO reto.hobbies VALUES ('1', 'Crossfit', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim');
+INSERT INTO reto.hobbies VALUES ('2', 'TRX', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim');
+INSERT INTO reto.hobbies VALUES ('3', 'Salsa Casino', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim');
+
+
+
+INSERT INTO reto.employee_performs_hobbies VALUES (1,1);
+INSERT INTO reto.employee_performs_hobbies VALUES (1,2);
+
+INSERT INTO reto.employee_performs_hobbies VALUES (2,3);
+INSERT INTO reto.employee_performs_hobbies VALUES (2,1);
+
+INSERT INTO reto.employee_performs_hobbies VALUES (3,2);
+INSERT INTO reto.employee_performs_hobbies VALUES (3,3);
+
+INSERT INTO reto.employee_performs_hobbies VALUES (4,1);
+INSERT INTO reto.employee_performs_hobbies VALUES (4,2);
