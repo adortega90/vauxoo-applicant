791b93d37eb949990b85ae5c84cf7d6a2e8b4db2
adortega90 <adortega90@gmail.com>
2016-12-03 19:04:59 -0430

    Creating tables of employee and employee_department

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -1,18 +1,18 @@
 CREATE SCHEMA reto;
 
-CREATE TABLE employee_department(
+CREATE TABLE reto.employee_department(
  	id serial PRIMARY KEY,
  	name VARCHAR (255) UNIQUE NOT NULL,
-	description VARCHAR (150) NOT NULL
+	description VARCHAR (350) NOT NULL
 	  
 );
-CREATE TABLE employee(
+CREATE TABLE reto.employee(
 	 id serial PRIMARY KEY,
-	 firs_name VARCHAR (50) UNIQUE NOT NULL,
+	 firs_name VARCHAR (50) NOT NULL,
 	 last_name VARCHAR (50) NOT NULL,
  	 department_id integer NOT NULL,
  	 CONSTRAINT employee_belongs_to_a_department_department_id_fkey FOREIGN KEY (department_id)
-      REFERENCES employee_department (id) MATCH SIMPLE
+      REFERENCES reto.employee_department (id) MATCH SIMPLE
       ON UPDATE CASCADE ON DELETE CASCADE
 );
 
@@ -23,7 +23,9 @@ INSERT INTO reto.employee_department VALUES ('4', 'Ventas', 'Lorem ipsum dolor s
 INSERT INTO reto.employee_department VALUES ('5', 'Estrategia', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim');
 INSERT INTO reto.employee_department VALUES ('6', 'Garantias', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim');
 
-INSERT INTO reto.employee_department VALUES (1, 'Luis', 'Garcia',6);
-INSERT INTO reto.employee_department VALUES (2, 'Erika', 'Pinedo',5);
-INSERT INTO reto.employee_department VALUES (3, 'Oriana', 'Castillo',4);
-INSERT INTO reto.employee_department VALUES (4, 'Cesar', 'Rodriguez',3);
+INSERT INTO reto.employee VALUES (1, 'Luis', 'Garcia',6);
+INSERT INTO reto.employee VALUES (2, 'Erika', 'Pinedo',5);
+INSERT INTO reto.employee VALUES (3, 'Oriana', 'Castillo',4);
+INSERT INTO reto.employee VALUES (4, 'Cesar', 'Rodriguez',3);
+
+
