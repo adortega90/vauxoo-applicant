ce0da6b12e8bb62a92953fbe4925dffceb7a44d6
adortega90 <adortega90@gmail.com>
2016-12-04 13:22:08 -0300

    Create function is_prime

--- primes.py
+++ primes.py
@@ -1,16 +1,19 @@
 """
-Your module documentation here
+Class * PrimeClass * is conformed by the function 
+is_prime that is in charge of
+ determining if a numer X is prime or not
 """
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    How to work function is_prime(num)
+   		if num is minor to 2 is_prime(num) return False	
     """
-
-    def is_prime(self, num_int):
-        """
-        Your method documentation here
-        """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+	def is_prime(num):
+	    if num < 2:     
+	        return False
+	    for i in range(2, num):  #A range from the number to the number choose
+	        if num % i == 0:    #If the rest gives 0 is not prime, therefore returns False
+	            return False
+	        return True    #Otherwise returns True
