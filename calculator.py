18c4ecbf13d42eb84a92a69aa2da740e6f0f741e
adortega90 <adortega90@gmail.com>
2016-12-04 13:26:10 -0300

    Create function sum

--- calculator.py
+++ calculator.py
@@ -1,16 +1,19 @@
 """
-Your module documentation here
+Class * CalculatorClass * is conformed by the function 
+sum(num_list),This function receives an array of numbers
+and returns the sum of all elements
 """
 
 
 class CalculatorClass(object):
-    """
-    Your class documentation here
+	"""
+    The function receives an * array * and traverses
+     it with a * for * and goes adding the elements 
+     until finishing and returning the cumulative summation.
     """
 
-    def sum(self, num_list):
-        """
-        Your method documentation here
-        """
-        # your sum code here
-        return "not implement yet"  # Remove this dummy line
+	def sum(num_list):
+		acum = 0
+		for i in num_list:
+			acum = i + acum
+		return acum 
