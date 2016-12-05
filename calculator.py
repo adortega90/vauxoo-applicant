fc1ffdf301c9cb6f49496f00db0b9db4ce4ed89f
adortega90 <adortega90@gmail.com>
2016-12-05 19:36:00 -0300

    PR error (3)

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 """
-Class  CalculatorClass  is conformed by the function 
+Class  CalculatorClass  is conformed by the function
 sum(num_list),This function receives an array of numbers
 and returns the sum of all elements
 """
@@ -8,16 +8,15 @@ and returns the sum of all elements
 class CalculatorClass(object):
     """
     The function receives an  array  and traverses
-    it with a  for  and goes adding the elements 
+    it with a  for  and goes adding the elements
     until finishing and returning the cumulative summation.
     """
 
     def sum(self, num_list):
         """
         num_list list of number
-        acum auxiliary 
+        acum auxiliary
         """
-        #start function
         acum = 0
         for num in num_list:
             acum = num + acum
--- primes.py
+++ primes.py
@@ -1,6 +1,6 @@
 """
-Class  PrimeClass  is conformed by the function 
-is_prime that is in charge of determining 
+Class  PrimeClass  is conformed by the function
+is_prime that is in charge of determining
 if a numer X is prime or not
 Import math library for use of the function floor
 """
@@ -9,16 +9,17 @@ import math
 class PrimeClass(object):
     """
     How to work function is_prime(num)
-    if num is minor to 2 is_prime(num) return False 
+    if num is minor to 2 is_prime(num) return False
     if num == 2 return True
     If none of the above is true, the number
     """
 
     def is_prime(self, num):
         """
-        num is the number a value
-        aux auxiliary value for operatons
+        :num is the number a value
+        :aux auxiliary value for operatons
         """
+        # Start evaluation
         if num < 2:
             return False
         elif num == 2:
@@ -30,5 +31,4 @@ class PrimeClass(object):
                     return False
                 else:
                     aux = aux-1
-                    
             return True
