1f318bf07f0d053b46c044c777d557f866b4a576
adortega90 <adortega90@gmail.com>
2016-12-05 18:33:39 -0300

    PR error (2)

--- calculator.py
+++ calculator.py
@@ -1,19 +1,24 @@
 """
-Class * CalculatorClass * is conformed by the function 
+Class  CalculatorClass  is conformed by the function 
 sum(num_list),This function receives an array of numbers
 and returns the sum of all elements
 """
 
 
 class CalculatorClass(object):
-	"""
-    The function receives an * array * and traverses
-     it with a * for * and goes adding the elements 
-     until finishing and returning the cumulative summation.
+    """
+    The function receives an  array  and traverses
+    it with a  for  and goes adding the elements 
+    until finishing and returning the cumulative summation.
     """
 
-	def sum(self, num_list):
-		acum = 0
-		for i in num_list:
-			acum = i + acum
-		return acum 
+    def sum(self, num_list):
+        """
+        num_list list of number
+        acum auxiliary 
+        """
+        #start function
+        acum = 0
+        for i in num_list:
+            acum = i + acum
+        return acum 
--- primes.py
+++ primes.py
@@ -1,37 +1,32 @@
 """
-Class * PrimeClass * is conformed by the function 
-is_prime that is in charge of
- determining if a numer X is prime or not
-"""
-
-"""
+Class  PrimeClass  is conformed by the function is_prime that is in charge of determining if a numer X is prime or not
 Import math library for use of the function floor
 """
 import math
 
 class PrimeClass(object):
+    """
+    How to work function is_prime(num)
+    if num is minor to 2 is_prime(num) return False 
+    if num == 2 return True
+    If none of the above is true, the number
+    """
 
-	"""
-	How to work function is_prime(num)
-	if num is minor to 2 is_prime(num) return False 
-	if num == 2 return True
-	If none of the above is true, the number
-	"""
-	def is_prime(self, num):
-	
-		"""
-			num is the number a value
-			aux Auxiliary value for operations
-		"""
-		if num < 2:
-			return False
-		elif num == 2:
-			return True
-		else:
-			aux = math.floor(num/2) # function of math 
-		while aux >= 2:
-			if num % aux == 0:
-				return False
-			else:
-				aux = aux-1
-		return True
+    def is_prime(self, num):
+        """
+        num is the number a value
+        aux auxiliary value for operations
+        """
+        if num < 2:
+            return False
+        elif num == 2:
+            return True
+        else:
+            aux = math.floor(num/2) 
+            while aux >= 2:
+                if num % aux == 0:
+                    return False
+                else:
+                    aux = aux-1
+                    
+            return True
